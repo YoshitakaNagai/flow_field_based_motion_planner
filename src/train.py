64033c40eea5fa09007ab4d3ed21004eb1f62e7f
YoshitakaNagai <basketball.1031.ndy@gmail.com>
2020-12-23 10:14:18 +0000

    temporal_bev

--- src/train.py
+++ src/train.py
@@ -17,9 +17,7 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseArray
 from nav_msgs.msg import Odometry
-from nav_msgs.msg import OccupancyGrid
 from std_msgs.msg import Bool
-from std_msgs.msg import Float32MultiArray
 from tf.transformations import euler_from_quaternion
 
 import cv2
@@ -46,13 +44,13 @@ Transition = namedtuple('Transition', ('state_m', 'state_g', 'state_v', 'action'
 
 ##### PARAM #####
 # for ALL
-maps = [None] * 3 # [0]:occupancy with robot, [1]:flow, [2]:occupancy without robot
+maps = [None]
 
 # for ROS
-IMAGE_SIZE = 40
+IMAGE_SIZE = 80
 MAP_GRID_SIZE = 0.1
-MAP_RANGE = 5.0
-ROBOT_RSIZE = 0.1
+MAP_RANGE = 8.0
+ROBOT_RSIZE = 0.13
 SLEEP_TIME = 10
 
 # for DDQN
@@ -65,7 +63,8 @@ BATCH_SIZE = 1024 # minibatch size
 # BATCH_SIZE = 128 # minibatch size
 CAPACITY = 200000 # replay buffer size
 # INPUT_CHANNELS = 12 #[channel] = (occupancy(MONO) + flow(RGB)) * series(3 steps)
-INPUT_CHANNELS = 3 #[channel] = (occupancy(MONO) + flow(xy)) * series(1 steps)
+# INPUT_CHANNELS = 3 #[channel] = (occupancy(MONO) + flow(xy)) * series(1 steps)
+INPUT_CHANNELS = 1 # only temporal_bev_image
 NUM_ACTIONS = 28
 LEARNING_RATE = 0.0005 # learning rate
 # LOSS_THRESHOLD = 0.1 # threshold of loss
@@ -78,9 +77,7 @@ MODEL_PATH = './model/model.pth'
 
 class ROSNode():
     def __init__(self):
-        self.sub_flow_x = rospy.Subscriber("/bev/flow_array_x", Float32MultiArray, self.flow_x_callback)
-        self.sub_flow_y = rospy.Subscriber("/bev/flow_array_y", Float32MultiArray, self.flow_y_callback)
-        self.sub_raycast = rospy.Subscriber("/bev/raycast_multiarray", Float32MultiArray, self.raycast_multiarray_callback)
+        self.sub_temporal_bev = rospy.Subscriber("/bev/temporal_bev_image", Image, self.temporal_bev_image_callback)
         self.sub_odom = rospy.Subscriber("/odom", Odometry, self.odom_callback)
         self.sub_start_goal = rospy.Subscriber("/start_goal_points", PoseArray, self.pose_array_callback)
         self.sub_laser = rospy.Subscriber("/scan", LaserScan, self.laser_callback)
@@ -98,16 +95,9 @@ class ROSNode():
         self.map_grid_size = MAP_GRID_SIZE
         self.map_range = MAP_RANGE
         self.robot_rsize = ROBOT_RSIZE
-        self.raycast = torch.zeros(IMAGE_SIZE, IMAGE_SIZE)
-        self.flow_x = torch.zeros(IMAGE_SIZE, IMAGE_SIZE)
-        self.flow_y = torch.zeros(IMAGE_SIZE, IMAGE_SIZE)
-        self.flow_xy = torch.zeros(2, IMAGE_SIZE, IMAGE_SIZE)
+        self.temporal_bev_map = torch.zeros(IMAGE_SIZE, IMAGE_SIZE)
 
-        self.flow_x_callback_flag = False
-        self.flow_y_callback_flag = False
-        self.flow_xy_callback_flag = False
-        # self.flow_callback_flag = False
-        self.raycast_callback_flag = False
+        self.temporal_bev_image_callback_flag = False
         self.odom_callback_flag = False
         self.posearray_callback_flag = False
         self.laser_callback_flag = False
@@ -116,78 +106,12 @@ class ROSNode():
     def is_start_callback(self, msg):
         self.is_start_callback_flag = True
 
-    # def occupancy_image_callback(self, msg):
-    #     # print("occupancy_image_callback")
-    #     cv_occupancy_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-    #     maps[2] = cv_occupancy_image.copy()
-    #
-    #     # tensor_occupancy_image = kornia.image_to_tensor(cv_occupancy_image, keepdim=True).float()
-    #     tensor_occupancy_image = torch.from_numpy(cv_occupancy_image)
-    #
-    #     for i in range(len(cv_occupancy_image[0])):
-    #         for j in range(len(cv_occupancy_image[1])):
-    #             if math.sqrt(math.pow(i * self.map_grid_size - 0.5 * self.map_range, 2) + math.pow(j * self.map_grid_size - 0.5 * self.map_range, 2)) <= self.robot_rsize:
-    #                 # cv_occupancy_image[i, j] = 1.0
-    #                 tensor_occupancy_image[i, j] = 1
-    #
-    #     maps[0] = tensor_occupancy_image
-    #     self.occupancy_callback_flag = True
-
-    def raycast_multiarray_callback(self, msg):
-        data_offset = msg.layout.data_offset
-        row = 0
-        col = 0
-        for i in range(len(msg.data)):
-            if (i + 1) % data_offset == 0:
-                col += 1
-            row = i - col * data_offset
-            if row < IMAGE_SIZE and col < IMAGE_SIZE:
-                self.raycast[row, col] = msg.data[i]
-                if math.sqrt(math.pow(row * self.map_grid_size - 0.5 * self.map_range, 2) + math.pow(col * self.map_grid_size - 0.5 * self.map_range, 2)) <= self.robot_rsize:
-                    self.raycast[row][col] = 1.0
-
-        maps[0] = self.raycast
-        self.raycast_callback_flag = True
-
-
-    # def flow_image_callback(self, msg):
-    #     # print("flow_image_callback")
-    #     cv_flow_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-    #     cv_flow_image = cv2.cvtColor(cv_flow_image, cv2.COLOR_BGR2RGB)
-    #     tensor_flow_image = kornia.image_to_tensor(cv_flow_image, keepdim=True).float()
-    #     # tensor_flow_image = torch.from_numpy(cv_flow_image)
-    #     maps[1] = tensor_flow_image
-    #     self.flow_callback_flag = True
-
-    def flow_x_callback(self, msg):
-        data_offset = msg.layout.data_offset
-        row = 0
-        col = 0
-        for i in range(len(msg.data)):
-            if (i + 1) % data_offset == 0:
-                col += 1
-            row = i - col * data_offset
-            if row < IMAGE_SIZE and col < IMAGE_SIZE:
-                self.flow_x[row][col] = msg.data[i]
-        self.flow_x_callback_flag = True
-
-    def flow_y_callback(self, msg):
-        data_offset = msg.layout.data_offset
-        row = 0
-        col = 0
-        for i in range(len(msg.data)):
-            if (i + 1) % data_offset == 0:
-                col += 1
-            row = i - col * data_offset
-            if row < IMAGE_SIZE and col < IMAGE_SIZE:
-                self.flow_y[row][col] = msg.data[i]
-        self.flow_y_callback_flag = True
-            
-    def concat_flow_xy(self):
-        flow_list = [self.flow_x, self.flow_y]
-        flow_xy = torch.cat(flow_list, 0)
-        maps[1] = self.flow_xy
-        self.flow_xy_callback_flag = True
+    def temporal_bev_image_callback(self, msg):
+        # print("temporal_bev_image_callback")
+        cv_temporal_bev_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        tensor_bev_image = kornia.image_to_tensor(cv_temporal_bev_image, keepdim=True).float()
+        self.temporal_bev_map = tensor_bev_image
+        self.temporal_bev_image_callback_flag = True
 
     def odom_callback(self, msg):
         # print("odom_callback")
@@ -279,11 +203,12 @@ class ReplayMemory(object):
 class Network(nn.Module):
     def __init__(self, input_channels, outputs):
         super(Network, self).__init__()
-        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=16)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=8)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3)
+        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=16)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=8)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=7)
         self.fc1 = nn.Linear(4, 64)
-        self.fc2 = nn.Linear(6400, 512)
+        self.fc2 = nn.Linear(5184, 512)
         self.fc3 = nn.Linear(512, 512)
         self.fc4_ea = nn.Linear(512, outputs) # Elements A(s,a) that depend on your actions.
         self.fc4_ev = nn.Linear(512, 1) # Elements V(s) that are determined only by the state.
@@ -307,7 +232,8 @@ class Network(nn.Module):
         # print("convw =", convw, ", convh =", convh)
         x_gv = torch.empty(BATCH_SIZE, convw, convh)
         
-        for i in range(64):
+        grid_num = convw
+        for i in range(grid_num):
             value = x_gv_[0][i].item()
             tile_gv = torch.full((convw, convh), value)
             x_gv = torch.stack(tuple(tile_gv), 0)
@@ -319,9 +245,9 @@ class Network(nn.Module):
         x_pls = x_m + x_gv
         x_pls = x_pls.to(device)
 
-        x_pls = F.relu(self.conv3(x_pls))
-        x_pls = F.relu(self.conv3(x_pls))
-        x_pls = F.relu(self.conv3(x_pls))
+        x_pls = F.relu(self.conv4(x_pls))
+        x_pls = F.relu(self.conv4(x_pls))
+        x_pls = F.relu(self.conv4(x_pls))
         # print("x_pls.size() = ", x_pls.size())
 
         ## batch_flattened_length = x_pls.shape[1] * x_pls.shape[2] * x_pls.shape[3]
@@ -506,39 +432,6 @@ class Environment:
         self.loss_convergence = False
         self.reach_rate = 0.0
 
-    def make_concat_map(self, occupancy_map, flow_map):
-        # occupancy_map.size() = torch.Size([H, W])
-        # flow_map.size() = torch.Size([3->2, H, W])
-
-        # print("occupancy_map.size() = ", occupancy_map.size())
-        occupancy_map = torch.unsqueeze(occupancy_map, 0)
-        # print("occupancy_map.size() = ", occupancy_map.size())
-        # print("flow_map.size() = ", flow_map.size())
-        
-        # occupancy_map.size() = torch.Size([1, H, W])
-        repeat_vals = (1, 40, 40)
-        concat_map = torch.cat((flow_map, occupancy_map.expand(*repeat_vals)), 0)
-        # print("concat_map.size() = ", concat_map.size())
-        # concat_map = torch.stack((flow_map, occupancy_map), 2)
-        # concat_map.size() = torch.Size([4->3, H, W])    
-        # concat_map = torch.unsqueeze(concat_map, 0)
-        
-        return concat_map
-    
-    def make_temporal_maps(self, concat_map, is_first):
-        if is_first:
-            self.map_memory.clear()
-            for i in range(3):
-                self.map_memory.append(concat_map)
-        else:
-            self.map_memory.append(concat_map)
-            del self.map_memory[0]
-        
-        temporal_maps = torch.cat(self.map_memory, 0)
-        # print("temporal_maps.size() = ", temporal_maps.size())
-        # temporal_maps.size() = torch.Size([12, H, W])
-        return temporal_maps
-
     def check_convergence(self):
         if len(self.loss_memory) > self.loss_memory_capacity:
             del self.loss_memory[0]
@@ -582,28 +475,19 @@ def main():
 
     print("ros : start!")
     while not rospy.is_shutdown():
-        if ros.flow_x_callback_flag and ros.flow_y_callback_flag:
-            ros.concat_flow_xy()
-        # print("[0] ros.flow_x_callback_flag:", ros.flow_x_callback_flag)
-        # print("[0] ros.flow_y_callback_flag:", ros.flow_y_callback_flag)
-        # print("[1] ros.flow_xy_callback_flag:", ros.flow_xy_callback_flag)
-        # print("[2] ros.raycast_callback_flag:", ros.raycast_callback_flag)
-        # print("[3] ros.odom_callback_flag:", ros.odom_callback_flag)
-        # print("[4] ros.posearray_callback_flag:", ros.posearray_callback_flag)
-        # print("[5] ros.laser_callback_flag:", ros.laser_callback_flag)
-        # if ros.flow_callback_flag and ros.occupancy_callback_flag and ros.odom_callback_flag and ros.posearray_callback_flag and ros.laser_callback_flag and ros.is_start_callback_flag:
-        if ros.flow_xy_callback_flag and ros.raycast_callback_flag and ros.odom_callback_flag and ros.posearray_callback_flag and ros.laser_callback_flag and ros.is_start_callback_flag:
+        # print("[0] ros.temporal_bev_image_callback_flag:", ros.temporal_bev_image_callback_flag)
+        # print("[1] ros.odom_callback_flag:", ros.odom_callback_flag)
+        # print("[2] ros.posearray_callback_flag:", ros.posearray_callback_flag)
+        # print("[3] ros.laser_callback_flag:", ros.laser_callback_flag)
+        if ros.temporal_bev_image_callback_flag  and ros.odom_callback_flag and ros.posearray_callback_flag and ros.laser_callback_flag and ros.is_start_callback_flag:
             print("ros flags : OK")
             robot_pose = ros.robot_position_extractor() # numpy
             relative_goal = ros.relative_goal_calculator(robot_pose) # numpy
             tmp_relative_goal = copy.deepcopy(relative_goal) # numpy
             velocity = ros.robot_velocity_calculator(robot_pose, is_first) # numpy
-            occupancy_map = maps[0] # tensor
-            flow_map = maps[1] #tensor
+            flow_map = ros.temporal_bev_map.clone() #tensor
 
-            concat_map = train_env.make_concat_map(occupancy_map, flow_map)
-            # observe_m = train_env.make_temporal_maps(concat_map, is_first)
-            observe_m = concat_map
+            observe_m = flow_map
             observe_m = torch.unsqueeze(observe_m, 0)
             observe_m = observe_m.to(device)
             # print("observe_m.size() = ", observe_m.size())
@@ -630,9 +514,6 @@ def main():
             action = train_env.agent.get_action(state_m, state_g, state_v, episode)
             action_id = action.item()
 
-            tmp_tensor_occupancy = occupancy_map.clone()
-            numpy_occupancy = tmp_tensor_occupancy.to('cpu').detach().numpy()
-            # numpy_reward, is_done = train_env.env.rewarder(numpy_occupancy, relative_goal, is_first)
             numpy_reward, is_done, is_goal = train_env.env.rewarder2(ros.scan_data, relative_goal, is_first)
 
             if is_goal:
@@ -655,10 +536,7 @@ def main():
             train_env.agent.memorize(state_m, state_g, state_v, action, observe_m, observe_g, observe_v, reward)
             train_env.agent.update_q_function()
 
-            ros.flow_xy_callback_flag = False
-            ros.flow_x_callback_flag = False
-            ros.flow_y_callback_flag = False
-            ros.raycast_callback_flag = False
+            ros.temporal_bev_image_callback_flag = False
             ros.odom_callback_flag = False
             ros.posearray_callback_flag = False
             ros.laser_callback_flag = False
